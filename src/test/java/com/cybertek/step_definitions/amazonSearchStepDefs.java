24a1155ef0334f5e6735c0f0a15728703ccbe331
Rasim Aghayev <rasim.ag1983@gmail.com>
2021-07-07 18:44:48 -0400

    cucumber

---
+++ src/test/java/com/cybertek/step_definitions/amazonSearchStepDefs.java
@@ -0,0 +1,39 @@
+package com.cybertek.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class amazonSearchStepDefs {
+
+    @Given("User is on the Amazon Home Page")
+    public void user_is_on_the_amazon_home_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+        @When("User search Hats for men")
+    public void user_search_hats_for_men() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("User adds the first hat appearing to the cart with quantity two")
+    public void user_adds_the_first_hat_appearing_to_the_cart_with_quantity_two() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the total price and quantity should be correct")
+    public void the_total_price_and_quantity_should_be_correct() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("User reduce the quantity from two to one")
+    public void user_reduce_the_quantity_from_two_to_one() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("total price and qyantity has been correctly change")
+    public void total_price_and_qyantity_has_been_correctly_change() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
